a895d315e0ec6d4bbfbe698edba7fe27575f327e
unknown <Administrator@REFRIPERU.INTRANET>
2025-07-09 11:52:40 -0500

    subiendo cambios part5

--- js/script.js
+++ js/script.js
@@ -3,24 +3,30 @@
       navLinks.classList.toggle('show');
     }
     
-        const topNav = document.querySelector('.top-nav');
-        const navbar = document.querySelector('.navbar');
-        const placeholder = document.querySelector('.placeholder-navbar');
+  const topNav = document.querySelector('.top-nav');
+  const navbar = document.querySelector('.navbar');
+  const placeholder = document.querySelector('.placeholder-navbar');
 
-        let lastScrollY = window.scrollY;
+  let lastScrollY = window.scrollY;
 
-        window.addEventListener('scroll', () => {
-        const currentScrollY = window.scrollY;
+  window.addEventListener('scroll', () => {
+    const currentScrollY = window.scrollY;
 
-        if (currentScrollY > lastScrollY && currentScrollY > 50) {
-            topNav.style.transform = 'translateY(-100%)';
-            navbar.classList.add('fixed');
-            placeholder.style.height = `${navbar.offsetHeight}px`;
-        } else {
-            topNav.style.transform = 'translateY(0)';
-            navbar.classList.remove('fixed');
-            placeholder.style.height = `0px`;
-        }
+    if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        topNav.style.transform = 'translateY(-100%)';
+        topNav.style.position = 'absolute';
+        topNav.style.top = '-100px';
 
-        lastScrollY = currentScrollY;
-        });
+        navbar.classList.add('fixed');
+        placeholder.style.height = `${navbar.offsetHeight}px`;
+    } else {
+        topNav.style.transform = 'translateY(0)';
+        topNav.style.position = 'relative';
+        topNav.style.top = '0';
+
+        navbar.classList.remove('fixed');
+        placeholder.style.height = `0px`;
+    }
+
+    lastScrollY = currentScrollY;
+  });
